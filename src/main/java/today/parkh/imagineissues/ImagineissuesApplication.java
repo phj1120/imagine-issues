a2efbb0a5a0f4d0d7cdd64766ce6ee090fac478a
phj1120 <hyeonj1998@naver.com>
2022-12-23 10:34:25 +0900

    Init Project

---
+++ src/main/java/today/parkh/imagineissues/ImagineissuesApplication.java
@@ -0,0 +1,13 @@
+package today.parkh.imagineissues;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImagineissuesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImagineissuesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/today/parkh/imagineissues/ImagineissuesApplicationTests.java
@@ -0,0 +1,13 @@
+package today.parkh.imagineissues;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImagineissuesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
