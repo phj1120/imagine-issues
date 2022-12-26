f098f9970d438c8c9acc3038f006ce8d928c013a
phj1120 <hyeonj1998@naver.com>
2022-12-26 23:19:21 +0900

    refactor(setting): 주요 key 다른 파일으로 관리

--- src/test/java/today/parkh/imagineissues/ApplicationSchedulerTest.java
+++ src/test/java/today/parkh/imagineissues/ApplicationSchedulerTest.java
@@ -13,7 +13,7 @@ class ApplicationSchedulerTest {
 
     @Test
     public void publish() {
-        String keyword = "캠핑 책스타그램 국내여행 타투 가울 운동 폰케이스 반려동물 셀카 패션스타그램 운동하는 부산 주얼리 부동산 감성 패션 요가 비건 필라테스 헬스타그램";
+        String keyword = "Cute, dog, play, happy, day, warm, painting drawing style";
         Post post = applicationScheduler.posting(keyword);
 
         System.out.println(post);
