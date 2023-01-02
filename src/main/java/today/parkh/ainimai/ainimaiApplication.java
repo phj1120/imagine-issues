2fba1a07ce3f0fc4acb5795a1f98b91fc64b07ff
phj1120 <hyeonj1998@naver.com>
2023-01-02 23:57:28 +0900

    fix(schedule): 스케줄링 설정을 위한 애노테이션 추가

--- src/main/java/today/parkh/ainimai/ainimaiApplication.java
+++ src/main/java/today/parkh/ainimai/ainimaiApplication.java
@@ -2,8 +2,10 @@ package today.parkh.ainimai;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class ainimaiApplication {
 
     public static void main(String[] args) {
